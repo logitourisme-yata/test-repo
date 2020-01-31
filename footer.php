f6dfb76563627ee907a1632d73b3b5bf9ea55b04
Tom <tom@yata.fr>
2020-01-31 15:11:24 +0100

    enleve mon coucou

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		</div><!-- #content -->
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
-			<h1>COUCOU</h1>
+			<h1></h1>
 			<div class="wrap">
 				<?php
 				get_template_part( 'template-parts/footer/footer', 'widgets' );
